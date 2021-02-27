dd1dc14f0d6b89740fcae76afeb4fd89d3012c89
Gideon Arom <gideonarom@Gideons-MBP.attlocal.net>
2021-02-27 09:46:32 -0800

    reoworking config

--- build.rb
+++ build.rb
@@ -13,12 +13,8 @@ post_data = JSON.parse(File.open(path + '/post_data/2021-02-23-test.json').read)
 @doc.at_css("title").content = post_data['title']
 @doc.at_css("h1").content = post_data['title']
 
-# convert post body content from markdown to html, create node, attach it to "main"
-puts post_data['body']
-puts "*******************"
+# convert post body content from markdown to html, attach it to "main"
 body = GitHub::Markup.render_s(GitHub::Markups::MARKUP_MARKDOWN, post_data['body'])
-body_content_node = Nokogiri::XML::Node.new(body, @doc)
-puts body
-main = @doc.at_css "main"
-body_content_node.parent = main
+@doc.at_css('main').add_child(body)
+
 File.write(path + '/public/posts/post2.html', @doc, mode: "w")
